ba9c5f937d70f058bb408bf2db4f886eaa8607e7
gerrkoff <antonkofa@gmail.com>
2017-11-26 22:01:30 +0400

    init

--- Studio404/Studio404.Web/Startup.cs
+++ Studio404/Studio404.Web/Startup.cs
@@ -33,7 +33,9 @@ namespace Studio404.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }

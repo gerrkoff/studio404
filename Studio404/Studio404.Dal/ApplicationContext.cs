258b5052feb148436e5483151b15398d3cfb4342
gerrkoff <antonkofa@gmail.com>
2017-11-28 01:16:01 +0400

    Set up db

---
+++ Studio404/Studio404.Dal/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studio404.Dal.Entity;
+
+namespace Studio404.Dal
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<BookingEntity> Bookings { get; set; }
+ 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=studio404;Username=antonprokofev");
+        }
+    }
+}

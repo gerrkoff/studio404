d208169be2ca5f575e92d299413f8a704702e17d
gerrkoff <antonkofa@gmail.com>
2017-12-02 13:56:15 +0400

    toolbar

--- Studio404/Studio404.Web/ClientApp/containers/root/App.jsx
+++ Studio404/Studio404.Web/ClientApp/containers/root/App.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Navigation from "../../components/root/Navigation";
 import LoginInfo from "../login/LoginInfo";
+import Toolbar from "../../components/root/Toolbar";
 
 class App extends Component {
     render() {
         return (
             <Container>
-                <Navigation />
+                <Toolbar />
                 <LoginInfo />
                 {this.props.children}
             </Container>

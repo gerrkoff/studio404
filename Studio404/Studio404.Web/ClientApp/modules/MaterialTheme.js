94df7f419e9ebf7c8ec20d7eb614687d3e627588
gerrkoff <antonkofa@gmail.com>
2017-12-06 01:55:11 +0400

    colors redesign

--- Studio404/Studio404.Web/ClientApp/modules/MaterialTheme.js
+++ Studio404/Studio404.Web/ClientApp/modules/MaterialTheme.js
@@ -1,15 +1,26 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {fade} from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
 import {
-    amberA200, amberA400
+    yellow600,
+    yellow800,
+    grey400,
+    blue500,
+    grey100,
+    grey500,
+    white,
+    grey300,
+    darkBlack,
+    fullBlack
 } from 'material-ui/styles/colors';
 
 export const muiTheme = getMuiTheme({
     
     palette: {
-        primary1Color: amberA200,
-        primary2Color: amberA400/*,
+        primary1Color: yellow600,
+        primary2Color: yellow800,
         primary3Color: grey400,
-        accent1Color: pinkA200,
+        accent1Color: blue500,
         accent2Color: grey100,
         accent3Color: grey500,
         textColor: darkBlack,
@@ -17,8 +28,8 @@ export const muiTheme = getMuiTheme({
         canvasColor: white,
         borderColor: grey300,
         disabledColor: fade(darkBlack, 0.3),
-        pickerHeaderColor: cyan500,
+        pickerHeaderColor: yellow600,
         clockCircleColor: fade(darkBlack, 0.07),
-        shadowColor: fullBlack,*/
+        shadowColor: fullBlack
     }
 });

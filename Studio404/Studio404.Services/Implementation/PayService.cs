64528dbde4f6e1d64cdf7c26c6de4451d5929a7e
gerrkoff <antonkofa@gmail.com>
2017-12-08 19:50:13 +0400

    refactor pay

--- Studio404/Studio404.Services/Implementation/PayService.cs
+++ Studio404/Studio404.Services/Implementation/PayService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Studio404.Common.Enums;
 using Studio404.Dal.Entity;
 using Studio404.Dal.Repository;
 using Studio404.Services.Interface;
@@ -17,14 +18,17 @@ namespace Studio404.Services.Implementation
 
         public void ConfirmBooking(Guid guid)
         {
-            int bookingId = _bookingRepository.GetAll()
-                .Where(x => x.Guid == guid)
-                .Select(x => x.Id)
-                .Single();
+            BookingEntity booking = _bookingRepository.GetAll()
+                .Single(x => x.Guid == guid);
 
-            string code = GenerateBookingCode();
-            var booking = new BookingEntity {Id = bookingId, Code = code};
-            _bookingRepository.SaveProperties(booking, x => x.Code);
+            if (booking.Status == BookingStatusEnum.Paid)
+            {
+                // TODO: Log warning if booking already paid                
+            }
+
+            booking.Code = GenerateBookingCode();
+            booking.Status = BookingStatusEnum.Paid;
+            _bookingRepository.Save(booking);
         }
 
         private string GenerateBookingCode()

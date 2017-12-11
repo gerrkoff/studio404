581232826632f3bf7898bc26fd7a19feec5b4047
SAMARA\Anton_Prokofiev1 <Anton_Prokofiev1@epam.com>
2017-12-11 13:39:32 +0400

    add logger

--- Studio404/Studio404.Web/Controllers/CheckController.cs
+++ Studio404/Studio404.Web/Controllers/CheckController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Studio404.Services.Interface;
 
 namespace Studio404.Web.Controllers
@@ -7,15 +8,18 @@ namespace Studio404.Web.Controllers
     public class CheckController : Controller
     {
         private readonly ICheckService _checkService;
+        private readonly ILogger<CheckController> _logger;
         
-        public CheckController(ICheckService checkService)
+        public CheckController(ICheckService checkService, ILogger<CheckController> logger)
         {
             _checkService = checkService;
+            _logger = logger;
         }
         
         [HttpGet("{shift}/{code}")]
         public bool Get(int shift, string code)
         {
+            _logger.LogInformation("Checking " + code);
             return _checkService.Check(shift, code);
         }
     }

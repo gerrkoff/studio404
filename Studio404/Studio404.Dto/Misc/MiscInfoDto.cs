72754909e6e41389a39c8abacdbe8c9dc147ded9
gerrkoff <antonkofa@gmail.com>
2017-12-18 02:41:51 +0400

    versions

---
+++ Studio404/Studio404.Dto/Misc/MiscInfoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Studio404.Dto.Misc
+{
+    public class MiscInfoDto
+    {
+        public string Version { get; set; }   
+    }
+}
---
+++ Studio404/Studio404.Web/Controllers/MiscController.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.PlatformAbstractions;
+using Studio404.Dto.Misc;
+using Studio404.Web.Controllers.Base;
+
+namespace Studio404.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class MiscController : BaseController
+    {
+        [HttpGet]
+        public MiscInfoDto Info()
+        {
+            return new MiscInfoDto
+            {
+                Version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                    .InformationalVersion
+            };
+        }
+    }
+}

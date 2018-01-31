c7f7d14f9c0cb962a7659d2e36302b0507fb9382
gerrkoff <antonkofa@gmail.com>
2018-02-01 01:01:10 +0400

    added db migration

---
+++ Studio404/Studio404.Dal/Migrations/20180131205617_Add_DisplayName_to_UserEntity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Studio404.Dal.Migrations
+{
+    public partial class Add_DisplayName_to_UserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+        }
+    }
+}

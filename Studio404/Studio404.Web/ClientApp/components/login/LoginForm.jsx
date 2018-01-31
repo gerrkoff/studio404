ffa4a87efc77485668f3b6c24cd3fe959e1a79be
SAMARA\Anton_Prokofiev1 <Anton_Prokofiev1@epam.com>
2018-01-31 15:33:05 +0400

    ext login - design

--- Studio404/Studio404.Web/ClientApp/components/login/LoginForm.jsx
+++ Studio404/Studio404.Web/ClientApp/components/login/LoginForm.jsx
@@ -27,11 +27,11 @@ class LoginForm extends Component {
                             errorText={this.props.loginInfo.passwordError} />
                     </Col>
                     <Col md="4">
-                        <FlatButton label="VKontakte" secondary={true} onClick={() => this.props.externalLogin("VKontakte")} /><br />
-                        <FlatButton label="Facebook" secondary={true} onClick={() => this.props.externalLogin("Facebook")} /><br />
-                        <FlatButton label="Twitter" secondary={true} onClick={() => this.props.externalLogin("Twitter")} /><br />
-                        <FlatButton label="Yandex" secondary={true} onClick={() => this.props.externalLogin("Yandex")} /><br />
-                        <FlatButton label="Google" secondary={true} onClick={() => this.props.externalLogin("Google")} /><br />
+                        <br /><FlatButton label="VKontakte" secondary={true} onClick={() => this.props.externalLogin("VKontakte")} />
+                        <br /><FlatButton label="Facebook" secondary={true} onClick={() => this.props.externalLogin("Facebook")} />
+                        <br /><FlatButton label="Twitter" secondary={true} onClick={() => this.props.externalLogin("Twitter")} />
+                        <br /><FlatButton label="Yandex" secondary={true} onClick={() => this.props.externalLogin("Yandex")} />
+                        <br /><FlatButton label="Google" secondary={true} onClick={() => this.props.externalLogin("Google")} />
                     </Col>
                 </Row>
             </div>

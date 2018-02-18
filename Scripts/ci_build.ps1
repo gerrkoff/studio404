fa9b0b9cf0143e7a620ae494e991f621699dd2ca
gerrkoff <antonkofa@gmail.com>
2018-02-18 14:06:44 +0400

    adjusted CI build script

--- Scripts/ci_build.ps1
+++ Scripts/ci_build.ps1
@@ -2,8 +2,6 @@ Remove-Item Deploy/src -recurse
 
 cd Studio404/Studio404.Web/
 
-Write-Output("TEST")
-
 npm install
 if ($LastExitCode -eq 1)
 {
@@ -13,7 +11,6 @@ if ($LastExitCode -eq 1)
 npm run build_p
 if ($LastExitCode -eq 1)
 {
-	Write-Output("EXITED")
 	exit 1
 }
 

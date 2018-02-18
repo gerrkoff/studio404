b91265654509024d34ab41bf4e49f46229609310
gerrkoff <antonkofa@gmail.com>
2018-02-18 13:33:01 +0400

    working on CI script

--- Scripts/ci_build.ps1
+++ Scripts/ci_build.ps1
@@ -2,5 +2,11 @@ Remove-Item Deploy/src -recurse
 
 cd Studio404/Studio404.Web/
 npm install
-npm run build_p
+$result = npm run build_p
+
+if ($result.ExitCode -eq 1)
+{
+	Write-Output("test")
+}
+
 dotnet publish -c Release -o ../../Deploy/src

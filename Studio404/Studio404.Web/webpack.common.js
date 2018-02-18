696f8b1ab004ac1003d88a6c40d8ee56f4ff3f6c
gerrkoff <antonkofa@gmail.com>
2018-02-18 13:24:05 +0400

    adjusted webpack to return error if build failed

--- Studio404/Studio404.Web/webpack.common.js
+++ Studio404/Studio404.Web/webpack.common.js
@@ -38,5 +38,15 @@ module.exports = {
                 loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
             }
         ]
-    }
+    },
+    plugins: [
+        function () {
+            this.plugin("done", function (stats) {
+                if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') == -1) {
+                    console.log(stats.compilation.errors);
+                    process.exit(1);
+                }
+            });
+        }
+    ]
 };

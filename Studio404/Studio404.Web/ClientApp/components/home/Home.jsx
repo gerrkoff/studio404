39d28c381295e7319dbe841b7108b63f2efe9772
gerrkoff <antonkofa@gmail.com>
2018-05-26 17:33:21 +0400

    added map, adjusted styles

--- Studio404/Studio404.Web/ClientApp/components/home/Home.jsx
+++ Studio404/Studio404.Web/ClientApp/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 import Paper from 'material-ui/Paper';
 import css from '../../styles/home.css';
+import { muiTheme } from '../../modules/MaterialTheme';
 
 class Home extends Component {
 
@@ -18,7 +19,7 @@ class Home extends Component {
                         <Paper className={ css.main } zDepth={1}>
                             <Row>
                                 <Col md="12">
-                                    <h4 className={ css.greeting }>Welcome to Studio 404!</h4>
+                                    <h4 className={ css.greeting } style={{color: muiTheme.palette.primary2Color}}>Welcome to Studio 404!</h4>
                                 </Col>
                             </Row>
                         </Paper>
@@ -26,11 +27,20 @@ class Home extends Component {
                 </Row>
                 <br/>
                 <Row>
-                    <Col md="12">
-                        <Paper className={ css.main } zDepth={1}>
+                    <Col md="4">
+                        <Paper className={ css.map } zDepth={1}>
                             <Row>
                                 <Col md="12">
-                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af4f7411f8e61f974863e86465b174241d4f4cbe9e07d8af8c1204659987f37d0&amp;source=constructor" width="100%" height="250px" frameborder="0"></iframe>
+                                    <h4 className={ css.greeting } style={{color: muiTheme.palette.primary2Color}}>Address</h4>
+                                </Col>
+                            </Row>
+                        </Paper>
+                    </Col>
+                    <Col md="8">
+                        <Paper className={ css.map } zDepth={1}>
+                            <Row>
+                                <Col md="12">
+                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af4f7411f8e61f974863e86465b174241d4f4cbe9e07d8af8c1204659987f37d0&amp;source=constructor" width="100%" height="250px" frameBorder="0"></iframe>
                                 </Col>
                             </Row>
                         </Paper>

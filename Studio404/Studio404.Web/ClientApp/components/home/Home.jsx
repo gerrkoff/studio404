877b1e5fe8971927d5a7bfed5feb4613b257ffc4
gerrkoff <antonkofa@gmail.com>
2018-05-27 12:17:43 +0400

    adjusted styles

--- Studio404/Studio404.Web/ClientApp/components/home/Home.jsx
+++ Studio404/Studio404.Web/ClientApp/components/home/Home.jsx
@@ -17,32 +17,15 @@ class Home extends Component {
                 <Row>
                     <Col md="12">
                         <Paper className={ css.main } zDepth={1}>
-                            <Row>
-                                <Col md="12">
-                                    <h4 className={ css.greeting } style={{color: muiTheme.palette.primary2Color}}>Welcome to Studio 404!</h4>
-                                </Col>
-                            </Row>
+                            <h4 className={ css.greeting } style={{color: muiTheme.palette.primary2Color}}>Welcome to Studio 404!</h4>
                         </Paper>
                     </Col>
                 </Row>
                 <br/>
                 <Row>
-                    <Col md="4">
+                    <Col md="12">
                         <Paper className={ css.map } zDepth={1}>
-                            <Row>
-                                <Col md="12">
-                                    <h4 className={ css.greeting } style={{color: muiTheme.palette.primary2Color}}>Address</h4>
-                                </Col>
-                            </Row>
-                        </Paper>
-                    </Col>
-                    <Col md="8">
-                        <Paper className={ css.map } zDepth={1}>
-                            <Row>
-                                <Col md="12">
-                                    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af4f7411f8e61f974863e86465b174241d4f4cbe9e07d8af8c1204659987f37d0&amp;source=constructor" width="100%" height="250px" frameBorder="0"></iframe>
-                                </Col>
-                            </Row>
+                            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af4f7411f8e61f974863e86465b174241d4f4cbe9e07d8af8c1204659987f37d0&amp;source=constructor" width="100%" height="250" frameBorder="0"></iframe>
                         </Paper>
                     </Col>
                 </Row>

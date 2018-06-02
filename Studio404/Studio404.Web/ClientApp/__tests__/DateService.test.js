a9a7ca9292244a700f450509bead4544827d5f07
gerrkoff <antonkofa@gmail.com>
2018-06-03 00:30:53 +0400

    fixed test

--- Studio404/Studio404.Web/ClientApp/__tests__/DateService.test.js
+++ Studio404/Studio404.Web/ClientApp/__tests__/DateService.test.js
@@ -2,6 +2,6 @@ const DateService = require('../modules/DateService')
 
 test('adds 3 days to 2018-06-04 equals to 2018-06-07', () => {
     let date = new Date(2018, 6, 4)
-    let result = DateService.default.addDaysToDate(date, 4)
+    let result = DateService.default.addDaysToDate(date, 3)
     expect(result.getTime()).toBe(new Date(2018, 6, 7).getTime())
 });

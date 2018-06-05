fbb9fd7ededebd2b5e3bfa930d7b74f98c266c67
gerrkoff <antonkofa@gmail.com>
2018-06-05 13:25:57 +0400

    removed dirty hack

--- Studio404/Studio404.Web/ClientApp/__tests__/DateService.test.js
+++ Studio404/Studio404.Web/ClientApp/__tests__/DateService.test.js
@@ -7,13 +7,13 @@ test('adds 3 days to 2018-06-04 equals to 2018-06-07', () => {
 })
 
 test('[toDateString] test', () => {
-    expect(DateService.toDateString('2018-06-03T14:00:00+05:00'))
+    expect(DateService.toDateString('2018-06-03T14:00:00'))
     // it's strange but that's it in Node.js environment
         .toBe('2018 M06 3')
 })
 
 test('[toTimeString] test passing string', () => {
-    expect(DateService.toTimeString('2018-06-03T14:53:00+05:00'))
+    expect(DateService.toTimeString('2018-06-03T14:53:00'))
         .toBe('14:53')
 })
 
@@ -23,11 +23,11 @@ test('[toTimeString] test passing date', () => {
 })
 
 test('[toTimeString] test midnight without 24h', () => {
-    expect(DateService.toTimeString('2018-06-03T00:00:00+05:00'))
+    expect(DateService.toTimeString('2018-06-03T00:00:00'))
         .toBe('00:00')
 })
 
 test('[toTimeString] test midnight with 24h conversion', () => {
-    expect(DateService.toTimeString('2018-06-03T00:00:00+05:00', true))
+    expect(DateService.toTimeString('2018-06-03T00:00:00', true))
         .toBe('24:00')
 })

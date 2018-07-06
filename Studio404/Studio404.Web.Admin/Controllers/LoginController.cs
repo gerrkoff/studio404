99871d8a1cf95876457528361b5ccffdb34d5172
gerrkoff <antonkofa@gmail.com>
2018-07-06 21:18:00 +0400

    added login controller

---
+++ Studio404/Studio404.Web.Admin/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Studio404.Web.Admin.Controllers
+{
+    [Route("/[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public FileResult Get()
+        {
+            return File("~/Login/index-login.html", "text/html");
+        }
+    }
+}

dfe42cfe296f13eb338a3cdf832ac301cdf77922
gerrkoff <antonkofa@gmail.com>
2018-07-06 22:38:00 +0400

    added denying all request except logging

---
+++ Studio404/Studio404.Web.Admin/Middleware/WhiteListMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Studio404.Web.Admin.Middleware
+{
+    public class WhiteListMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly PathString _pathStartsWith;
+        private readonly string _policyName;
+    
+        public WhiteListMiddleware(RequestDelegate next, WhiteListOptions options)
+        {
+            _next = next;
+            _pathStartsWith = options.PathStartsWith;
+            _policyName = options.PolicyName;
+        }
+ 
+        public async Task Invoke(HttpContext httpContext, IAuthorizationService authorizationService)
+        {
+            if (!httpContext.Request.Path.StartsWithSegments(_pathStartsWith))
+            {
+                AuthorizationResult authorized =
+                    await authorizationService.AuthorizeAsync(httpContext.User, null, _policyName);
+
+                if (!authorized.Succeeded)
+                {
+                    await httpContext.ChallengeAsync();
+                    return;
+                }
+            }
+            
+            await _next(httpContext);
+        }
+    }
+
+    public class WhiteListOptions
+    {
+        public PathString PathStartsWith { get; set; }
+        public string PolicyName { get; set; }
+    }
+}
--- Studio404/Studio404.Web.Admin/Startup.cs
+++ Studio404/Studio404.Web.Admin/Startup.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Studio404.Web.Admin.Middleware;
+using Microsoft.AspNetCore.Http;
 
 namespace Studio404.Web.Admin
 {
     public class Startup
     {
+        private const string AuthorizedUsersPolicyName = "IsAdmin";
+        private const string LoginPath = "/login";
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,6 +29,18 @@ namespace Studio404.Web.Admin
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new PathString(LoginPath);
+                });
+            
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(AuthorizedUsersPolicyName, policy => policy.RequireAuthenticatedUser());
+            });
+            
             services.AddMvc();
         }
 
@@ -34,8 +52,14 @@ namespace Studio404.Web.Admin
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMiddleware<WhiteListMiddleware>(new WhiteListOptions()
+            {
+                PathStartsWith = LoginPath,
+                PolicyName = AuthorizedUsersPolicyName
+            });
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }

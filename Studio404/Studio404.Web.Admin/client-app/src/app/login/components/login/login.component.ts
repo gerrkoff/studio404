0c0daa75ce710a10f05ea48428e8b32679a77bc0
gerrkoff <antonkofa@gmail.com>
2018-07-07 12:57:00 +0400

    working on login

--- Studio404/Studio404.Web.Admin/client-app/src/app/login/components/login/login.component.ts
+++ Studio404/Studio404.Web.Admin/client-app/src/app/login/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  validateForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
   }
 
+  onSubmitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
 }
--- Studio404/Studio404.Web.Admin/client-app/src/app/login/login.module.ts
+++ Studio404/Studio404.Web.Admin/client-app/src/app/login/login.module.ts
@@ -2,9 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
+
 @NgModule({
   declarations: [LoginComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [LoginComponent]
 })
 export class LoginModule { }

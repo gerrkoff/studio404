a6ee441036bde3b5f41a17179e0dd41222e221fe
SAMARA\Anton_Prokofiev1 <Anton_Prokofiev1@epam.com>
2018-07-10 15:47:46 +0400

    working on user bookings

--- Studio404/Studio404.Web.Admin/client-app/src/app/bookings/components/user-bookings/user-bookings.component.ts
+++ Studio404/Studio404.Web.Admin/client-app/src/app/bookings/components/user-bookings/user-bookings.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TableComponent } from '../../../common/components/table.component';
+import { BookingUser } from '../../models/booking-user';
+import { BookingsService } from '../../services/bookings.service';
 
 @Component({
   selector: 'app-user-bookings',
   templateUrl: './user-bookings.component.html',
-  styleUrls: ['./user-bookings.component.css']
+  styleUrls: [
+    './user-bookings.component.css',
+    '../../../common/styles/table.css'
+  ]
 })
-export class UserBookingsComponent implements OnInit {
+export class UserBookingsComponent extends TableComponent<BookingUser> {
+  
+  itemSearchFieldName = 'userDisplayName';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private bookingsService: BookingsService
+  ) {
+    super();
   }
 
+  loadItemsCore(): Promise<BookingUser[]> {
+    return this.bookingsService.getUserBookings();
+  }
 }

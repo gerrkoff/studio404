0a53b055f050a6537759b8cd9ca9d04357bbf512
gerrkoff <antonkofa@gmail.com>
2018-07-11 14:51:52 +0400

    added login controller to admin

--- Studio404/Studio404.Web.Admin/Controllers/LoginController.cs
+++ Studio404/Studio404.Web.Admin/Controllers/LoginController.cs
@@ -1,18 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Studio404.Common.Enums;
+using Studio404.Dto.Account;
+using Studio404.Services.Interface;
+using Studio404.Web.Controllers.Base;
 
 namespace Studio404.Web.Admin.Controllers
 {
     [Route("/[controller]")]
-    public class LoginController : Controller
+    public class LoginController : BaseController
     {
+        private readonly IAccountService _accountService;
+
+        public LoginController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
         [HttpGet]
         public FileResult Get()
         {
             return File("~/Login/index-login.html", "text/html");
+        }
+
+        [HttpPost]
+        public Task<LoginResultEnum> Post(LoginInfoDto loginInfo)
+        {
+            Validate();
+            return _accountService.LoginCookie(loginInfo);
+
+        }
+
+        [HttpDelete]
+        public Task Delete()
+        {
+            return _accountService.LogoutCookie();
         }
     }
 }

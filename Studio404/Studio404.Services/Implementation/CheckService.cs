42f71f254f30d132cd81cffd72e65d2bd7a391b3
gerrkoff <antonkofa@gmail.com>
2018-07-11 11:49:50 +0400

    added logging for researching about bug

--- Studio404/Studio404.Services/Implementation/CheckService.cs
+++ Studio404/Studio404.Services/Implementation/CheckService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Microsoft.Extensions.Logging;
 using Studio404.Dal.Entity;
 using Studio404.Dal.Repository;
 using Studio404.Services.Interface;
@@ -10,16 +11,19 @@ namespace Studio404.Services.Implementation
 	{
 		private readonly IRepository<BookingEntity> _bookingRepository;
 		private readonly IDateService _dateService;
+		private readonly ILogger<CheckService> _logger;
 
-		public CheckService(IRepository<BookingEntity> bookingRepository, IDateService dateService)
+		public CheckService(IRepository<BookingEntity> bookingRepository, IDateService dateService, ILogger<CheckService> logger = null)
 		{
 			_bookingRepository = bookingRepository;
 			_dateService = dateService;
+			_logger = logger;
 		}
 
 		public bool Check(int shiftMinutes, string code)
 		{
 			var now = _dateService.NowUtc.AddMinutes(shiftMinutes);
+			_logger?.LogInformation($"Now equals to {now}");
 			return _bookingRepository.GetAll()
 				.Any(x =>
 						(x.Status == BookingStatusEnum.Special || x.Status == BookingStatusEnum.Paid) &&

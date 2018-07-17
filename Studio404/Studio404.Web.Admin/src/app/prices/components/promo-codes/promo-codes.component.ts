00eba24171df029d23f5dfc53e8a8e43f72c98a9
SAMARA\Anton_Prokofiev1 <Anton_Prokofiev1@epam.com>
2018-07-17 18:51:48 +0400

    working on promo codes view

--- Studio404/Studio404.Web.Admin/src/app/prices/components/promo-codes/promo-codes.component.ts
+++ Studio404/Studio404.Web.Admin/src/app/prices/components/promo-codes/promo-codes.component.ts
@@ -30,12 +30,14 @@ export class PromoCodesComponent extends TableEditableComponent<PromoCode> {
   }
 
   protected createNewItem(): PromoCode {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
     return {
       code: '',
       description: '',
       discount: 0,
-      from: new Date(),
-      to: new Date(),
+      from: today,
+      to: today,
       id: 0
     };
   }
@@ -50,7 +52,29 @@ export class PromoCodesComponent extends TableEditableComponent<PromoCode> {
 
   protected validate(id: number): boolean {
     const row = this.table.rows[id];
+
+    row.fieldInvalid['discount'] = false;
+    row.fieldInvalid['from'] = false;
+    row.fieldInvalid['to'] = false;    
     row.fieldInvalid['code'] = row.data.code.length === 0;
-    return !row.fieldInvalid['code'];
+
+    if(row.data.discount.toString() === '') {
+      row.fieldInvalid['discount'] = true;
+    }
+
+    if(!row.data.from) {
+      row.fieldInvalid['from'] = true;
+    }
+
+    if(!row.data.to) {
+      row.fieldInvalid['to'] = true;
+    }
+
+    if(row.data.to && row.data.from && row.data.from > row.data.to) {
+      row.fieldInvalid['from'] = true;
+      row.fieldInvalid['to'] = true;
+    }
+
+    return !row.fieldInvalid['code'] && !row.fieldInvalid['discount'];
   }
 }

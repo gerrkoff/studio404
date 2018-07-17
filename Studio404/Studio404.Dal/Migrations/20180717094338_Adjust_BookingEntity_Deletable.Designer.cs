ff3ba97efa1c97c155439f8174a12d2a6fe6d889
SAMARA\Anton_Prokofiev1 <Anton_Prokofiev1@epam.com>
2018-07-17 13:45:18 +0400

    added migration to mask booking deletable

--- Studio404/Studio404.Dal/Migrations/20180717094338_Adjust_BookingEntity_Deletable.Designer.cs
+++ Studio404/Studio404.Dal/Migrations/20180717094338_Adjust_BookingEntity_Deletable.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Studio404.Dal.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20180717094338_Adjust_BookingEntity_Deletable")]
+    partial class Adjust_BookingEntity_Deletable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Studio404/Studio404.Dal/Migrations/20180717094338_Adjust_BookingEntity_Deletable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Studio404.Dal.Migrations
+{
+    public partial class Adjust_BookingEntity_Deletable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bookings",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bookings");
+        }
+    }
+}

1c5697a58045c0363e577ab245ab60b3234bb27b
gerrkoff <antonkofa@gmail.com>
2018-07-26 14:34:47 +0400

    fixed promo codes working with dates

--- Studio404/Studio404.Web.Admin/src/app/prices/services/promo-codes.service.ts
+++ Studio404/Studio404.Web.Admin/src/app/prices/services/promo-codes.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, noop } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { PromoCode } from '../models/promo-code';
 import { AppHttpClientService } from '../../common/services/app-http-client.service';
-
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +16,28 @@ export class PromoCodesService {
   ) {}
 
   getPromoCodes(): Observable<PromoCode[]> {
-    return this.http.get<PromoCode[]>(this.URL);
+    return this.http.get<PromoCode[]>(this.URL).pipe(map(this.fixDates));
   }
 
   savePromoCode(booking: PromoCode): Observable<PromoCode> {
+    booking.from = this.convertToUTC(booking.from);
+    booking.to = this.convertToUTC(booking.to);
     return this.http.post<PromoCode>(this.URL, booking);
   }
 
   deletePromoCode(id: number): Observable<void> {
     return this.http.delete<void>(`${this.URL}/${id}`);
+  }
+
+  private convertToUTC(date: Date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds()));
+  }
+
+  private fixDates(codes: PromoCode[]): PromoCode[] {
+    codes.forEach(x => {
+      x.from = new Date(x.from);
+      x.to = new Date(x.to);
+    });
+    return codes;
   }
 }

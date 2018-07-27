d914fde657296b59c7050248998ce9547216eb0f
SAMARA\Anton_Prokofiev1 <Anton_Prokofiev1@epam.com>
2018-07-27 20:30:37 +0400

    adjusted styles

--- Studio404/Studio404.Web/ClientApp/components/resetPassword/ResetPassword.jsx
+++ Studio404/Studio404.Web/ClientApp/components/resetPassword/ResetPassword.jsx
@@ -4,11 +4,12 @@ import RaisedButton from 'material-ui/RaisedButton'
 import FlatButton from 'material-ui/FlatButton'
 import { Step, Stepper, StepLabel, StepContent } from 'material-ui/Stepper'
 import Labels from '../../modules/Labels'
+import css from '../../styles/resetPassword.css'
 
 class ResetPassword extends Component {
     render () {
         return (
-            <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+            <div className={css.container}>
                 <Stepper activeStep={this.props.step} orientation="vertical">
                     <Step>
                         <StepLabel>{Labels.resetPass_step1}</StepLabel>
@@ -29,69 +30,75 @@ class ResetPassword extends Component {
 
     renderStep1 () {
         return (
-            <div>
-                <TextField
-                    hintText={Labels.loginForm_userEnter}
-                    floatingLabelText={Labels.loginForm_user}
-                    value={this.props.step1.username}
-                    onChange={e => this.props.updateStep1Info('username', e.target.value)}
-                    errorText={this.props.step1.usernameError} />
-                <br />
-                <RaisedButton
-                    label={Labels.resetPass_sendToken}
-                    primary={true}
-                    onClick={() => this.props.sendResetPassToken(this.props.step1.username)}
-                    disabled={!this.props.step1.valid || this.props.step1.processing} />
+            <div className={css.content}>
+                <div className={css.form}>
+                    <TextField
+                        hintText={Labels.loginForm_userEnter}
+                        floatingLabelText={Labels.loginForm_user}
+                        value={this.props.step1.username}
+                        onChange={e => this.props.updateStep1Info('username', e.target.value)}
+                        errorText={this.props.step1.usernameError} />
+                    <br />
+                    <RaisedButton
+                        className={css.buttonStacked}
+                        label={Labels.resetPass_sendToken}
+                        primary={true}
+                        onClick={() => this.props.sendResetPassToken(this.props.step1.username)}
+                        disabled={!this.props.step1.valid || this.props.step1.processing} />
+                </div>
             </div>
         )
     }
 
     renderStep2 () {
         return (
-            <div>
+            <div className={css.content}>
                 <p>{Labels.resetPass_codeWasSent}</p>
-                <TextField
-                    hintText={Labels.resetPass_tokenEnter}
-                    floatingLabelText={Labels.resetPass_token}
-                    value={this.props.step2.token}
-                    onChange={e => this.props.updateStep2Info('token', e.target.value)}
-                    errorText={this.props.step2.tokenError} />
-                <br />
-                <TextField
-                    hintText={Labels.changePass_newPassEnter}
-                    floatingLabelText={Labels.changePass_newPass}
-                    type="password"
-                    value={this.props.step2.newPassword}
-                    onChange={e => this.props.updateStep2Info('newPassword', e.target.value)}
-                    errorText={this.props.step2.newPasswordError} />
-                <br />
-                <TextField
-                    hintText={Labels.changePass_newPassConfirmEnter}
-                    floatingLabelText={Labels.changePass_newPassConfirm}
-                    type="password"
-                    value={this.props.step2.newPasswordConfirm}
-                    onChange={e => this.props.updateStep2Info('newPasswordConfirm', e.target.value)}
-                    errorText={this.props.step2.newPasswordConfirmError} />
-                <br />
-                <RaisedButton
-                    label={Labels.resetPassword}
-                    primary={true}
-                    onClick={() => this.props.resetPass({
-                        userId: this.props.step1.username,
-                        token: this.props.step2.token,
-                        newPassword: this.props.step2.newPassword
-                    })}
-                    disabled={!this.props.step2.valid || this.props.step2.processing} />
-                <FlatButton
-                    label={Labels.back}
-                    onClick={() => this.props.stepBack()} />
+                <div className={css.form}>
+                    <TextField
+                        hintText={Labels.resetPass_tokenEnter}
+                        floatingLabelText={Labels.resetPass_token}
+                        value={this.props.step2.token}
+                        onChange={e => this.props.updateStep2Info('token', e.target.value)}
+                        errorText={this.props.step2.tokenError} />
+                    <br />
+                    <TextField
+                        hintText={Labels.changePass_newPassEnter}
+                        floatingLabelText={Labels.changePass_newPass}
+                        type="password"
+                        value={this.props.step2.newPassword}
+                        onChange={e => this.props.updateStep2Info('newPassword', e.target.value)}
+                        errorText={this.props.step2.newPasswordError} />
+                    <br />
+                    <TextField
+                        hintText={Labels.changePass_newPassConfirmEnter}
+                        floatingLabelText={Labels.changePass_newPassConfirm}
+                        type="password"
+                        value={this.props.step2.newPasswordConfirm}
+                        onChange={e => this.props.updateStep2Info('newPasswordConfirm', e.target.value)}
+                        errorText={this.props.step2.newPasswordConfirmError} />
+                    <br />
+                    <RaisedButton
+                        className={css.buttonStacked}
+                        label={Labels.resetPassword}
+                        primary={true}
+                        onClick={() => this.props.resetPass({
+                            userId: this.props.step1.username,
+                            token: this.props.step2.token,
+                            newPassword: this.props.step2.newPassword
+                        })}
+                        disabled={!this.props.step2.valid || this.props.step2.processing} />
+                    <FlatButton
+                        label={Labels.back}
+                        onClick={() => this.props.stepBack()} />
+                </div>
             </div>
         )
     }
 
     renderStep3 () {
         return (
-            <div>{Labels.resetPass_finish}</div>
+            <div className={css.content}>{Labels.resetPass_finish}</div>
         )
     }
 }

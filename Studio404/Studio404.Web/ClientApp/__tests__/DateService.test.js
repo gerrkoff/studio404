ef17065080fe0e475510b730003a06ee22f350e4
gerrkoff <antonkofa@gmail.com>
2018-12-29 19:58:22 +0300

    Revert "Update DateService.test.js"
    This reverts commit 6625439816e54e143193ae0b38c6b0ba152002d2.

--- Studio404/Studio404.Web/ClientApp/__tests__/DateService.test.js
+++ Studio404/Studio404.Web/ClientApp/__tests__/DateService.test.js
@@ -5,13 +5,13 @@ test('adds 3 days to 2018-06-04 equals to 2018-06-07', () => {
     let result = DateService.addDaysToDate(date, 3)
     expect(result.getTime()).toBe(new Date(2018, 6, 7).getTime())
 })
-/*
+
 test('[toDateString] test', () => {
     expect(DateService.toDateString('2018-06-03T14:00:00'))
     // it's strange but that's it in Node.js environment
         .toBe('2018 M06 3')
 })
-*/
+
 test('[toTimeString] test passing string', () => {
     expect(DateService.toTimeString('2018-06-03T14:53:00'))
         .toBe('14:53')

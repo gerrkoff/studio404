76383ce743dcf01bfd7fe26228c2b42ecd3c7828
gerrkoff <antonkofa@gmail.com>
2019-06-26 09:48:56 +0300

    wip

---
+++ Scripts/bump-version.sh
@@ -0,0 +1,27 @@
+echo
+echo "      ─▄▀─▄▀"
+echo "      ──▀──▀"
+echo "      █▀▀▀▀▀█▄     --------------------------------"
+echo "      █░░░░░█─█    | It's time to update version! |"
+echo "      ▀▄▄▄▄▄▀▀     --------------------------------"
+echo
+echo "Enter WEB version (PATCH) ('-' for skip): " 
+read versionWeb
+echo "Enter ADMIN version (PATCH) ('-' for skip): " 
+read versionAdmin
+
+echo "WEB"
+if [ ! -z "$versionWeb" && $versionWeb == '-' ]
+    then
+        echo "Skip version"
+    else
+        dotnet dotnet-bumpversion/BumpVersion.dll ../Studio404/Studio404.Web/Studio404.Web.csproj $versionWeb
+fi
+
+echo "ADMIN"
+if [ ! -z "$versionAdmin" && $versionAdmin == '-' ]
+    then
+        echo "Skip version"
+    else
+        dotnet dotnet-bumpversion/BumpVersion.dll ../Studio404/Studio404.Web.Admin/Studio404.Web.Admin.csproj $versionAdmin
+fi

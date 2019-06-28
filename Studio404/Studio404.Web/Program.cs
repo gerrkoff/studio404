184580176158ed9a2d6b9c0285e3e191103a4f9e
gerrkoff <antonkofa@gmail.com>
2019-06-28 10:11:18 +0300

    fix

--- Studio404/Studio404.Web/Program.cs
+++ Studio404/Studio404.Web/Program.cs
@@ -10,7 +10,7 @@ namespace Studio404.Web
         public static void Main(string[] args)
         {
             // NLog: setup the logger first to catch all errors
-            var logger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
             try
             {
                 logger.Debug("init main");
